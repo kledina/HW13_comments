dceb3c6ab153ab11908b46b9bc1614693210cf2d
kledina <kledina@yandex.ru>
2023-02-26 14:22:36 +0300

    update

--- script.js
+++ script.js
@@ -2,6 +2,18 @@ let inputfield = document.getElementsByClassName('inputfield');
 function capitalizeName(str) {
     return str[0].toUpperCase() + str.toLowerCase().slice(1);
 }
+
+function checkYes(updatedUsername) {
+    let checkbox = document.getElementById('yes');
+    if(checkbox.checked === false)
+    {
+        inputfield[0].value = "Username";
+    }
+    else {
+        inputfield[0].value = updatedUsername;
+    }
+}
+
 const postIt = () => {
     let username = document.getElementById('username').value.replace('/\s+/g', ' ').trim();
     let avatar = document.getElementById('avatar').value;
@@ -11,47 +23,42 @@ const postIt = () => {
     usernameArray.forEach((item) => {
         updatedUsername = capitalizeName(item) + ' ' + updatedUsername;
     })
-    function checkYes() {
-        let checkbox = document.getElementById('yes');
-        if(checkbox.checked!=false)
-        {
-            inputfield[0].value = "Username";
-        }
-        else {
-            inputfield[0].value = updatedUsername;
-        }
-    }
+    checkYes(updatedUsername);
     
     let date = Date.now();
 
     function leadingZero(token) {
         return ("0" + token).slice(-2)
     }
-    
-    let weekday = date.getDay(),
-        year = date.getFullYear(),
-        month = leadingZero(date.getMonth() + 1),
-        day = leadingZero(date.getDate()),
-        hours = leadingZero(date.getHours()),
-        minutes = leadingZero(date.getMinutes())
-    
-    let currentDate = `${weekday}, ${day}.${month}.${year} в ${hours}:${minutes}`;
-    document.getElementById('date').value = currentDate;
 
-    let avatarSrc = document.getElementById('avatar').value;
+    let days = ['вс', 'пн', 'вт', 'ср', 'чт', 'пт', 'сб'];
+
+    let weekday = days[(new Date (date)).getDay()];
+        year = (new Date (date)).getFullYear(),
+        month = leadingZero((new Date (date)).getMonth() + 1),
+        day = leadingZero((new Date (date)).getDate()),
+        hours = leadingZero((new Date (date)).getHours()),
+        minutes = leadingZero((new Date (date)).getMinutes())
+    
+//var myDate = new Date();
+//var fullDate = "Сегодня: " + myDate.getDate() + " " + months[myDate.getMonth()] + 
+    //" " + myDate.getFullYear() + ", " + days[myDate.getDay()];
+    let currentDate = `${weekday}, ${day}.${month}.${year} в ${hours}:${minutes}`;
+    document.getElementById('date').innerHTML = currentDate;
+
     let avatarImage = document.getElementById('pic');
     let images = [
-        '../assets/baby.jpg',
-        '../assets/bird.jpg',
-        '../assets/brad.jpg',
-        '../assets/smile.jpg',
-        '../assets/lemon.jpg',
+        './assets/baby.jpg',
+        './assets/bird.jpg',
+        './assets/brad.jpg',
+        './assets/smile.jpg',
+        './assets/lemon.jpg',
     ];
-    if (avatarSrc = '') {
+    if (avatar === '') {
         avatarImage.src = images[Math.floor(Math.random()*images.length)];
     }
     else {
-        avatarImage.src = avatarSrc;
+        avatarImage.src = avatar;
     }
     
     newMessage.value = message.replace(/viagra|xxx/gi, '***');
